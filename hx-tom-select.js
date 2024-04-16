e619fdd72891892707f80f005d78167b20b66325
KiwiKid <KiwiKid@users.noreply.github.com>
2024-04-17 09:38:34 +1200

    more logging

--- hx-tom-select.js
+++ hx-tom-select.js
@@ -1,16 +1,21 @@
+const logIt = (msg) => {
+    console.log(`hx-tomselect: ${msg}`)
+} 
+
+
 htmx.defineExtension('tomselect', {
     transformResponse : function(text, xhr, elt) { console.log('transformResponse'); return text;},
     isInlineSwap : function(swapStyle) {  console.log('isInlineSwap'); return false;},
     handleSwap : function(swapStyle, target, fragment, settleInfo) {console.log('handleSwap');  return false;},
     encodeParameters : function(xhr, parameters, elt) {console.log('encodeParameters');  return null;},
     onEvent: function(name, evt) {
-        console.log(`onEvent - ${name}`)
+        logIt(`onEvent - ${name}`)
         if (name === "htmx:afterProcessNode") {
-            console.log(`htmx:afterProcessNode ${name}`)
+            logIt(`htmx:afterProcessNode ${name}`)
             document.querySelectorAll('[hx-ext=tomselect]').forEach(element => {
                 try {
 
-                    console.log('setting up new tomselect for '+element)
+                    logIt('setting up new tomselect for '+element)
                     if (!element.classList.contains('tomselect-initialized')) {
                         let config = {
                             maxOptions: element.getAttribute('max-options') || 100,
@@ -42,7 +47,7 @@ htmx.defineExtension('tomselect', {
                             let rawConfig = JSON.parse(element.getAttribute('raw-config'));
                             config = {...config, ...rawConfig};
                         }
-
+                        logIt('hx-tomselect: config', config)
                         initializeTomSelect(element, config);
                         element.classList.add('tomselect-initialized');
                     }
@@ -53,4 +58,4 @@ htmx.defineExtension('tomselect', {
         }
     }
 });
-console.log('hx tomselect loaded!')
+logIt('hx-tomselect: loaded!')
