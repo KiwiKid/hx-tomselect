c278bca96fdc6ecce025eb05210169ff2ee844cc
KiwiKid <KiwiKid@users.noreply.github.com>
2024-04-18 23:06:35 +1200

    improve workingness

--- hx-tom-select.js
+++ hx-tom-select.js
@@ -1,57 +1,54 @@
 htmx.defineExtension('tomselect', {
-    transformResponse : function(text, xhr, elt) { console.info('transformResponse'); return text;},
-    isInlineSwap : function(swapStyle) {  console.info('isInlineSwap'); return false;},
-    handleSwap : function(swapStyle, target, fragment, settleInfo) {console.info('handleSwap');  return false;},
-    encodeParameters : function(xhr, parameters, elt) {console.info('encodeParameters');  return null;},
-    onEvent: function(name, evt) {
-        console.info(`htmx-tomselect - onEvent - ${name}`)
-        if (name === "isInlineSwap") {
-            console.info(`htmx-tomselect - htmx:afterProcessNode ${name}`)
-            document.querySelectorAll('[hx-ext=tomselect]').forEach(element => {
-                try {
+    onEvent: function (name, evt) {
+        if (name === "htmx:afterProcessNode") {
+            try {
+                var elt = evt.detail.elt;
+                const selects = document.querySelectorAll('select[hx-ext="tomselect"]:not([tom-select-success]):not([tom-select-error])')
+                console.log('processing!'+selects.length)
+                selects.forEach((s) => {
+                    let config = {
+                        maxOptions: s.getAttribute('max-options') || 100,
+                        plugins: {}
+                    };
 
-                    console.log('htmx-tomselect - setting up new tomselect for '+element)
-                    if (!element.classList.contains('tomselect-initialized')) {
-                        let config = {
-                            maxOptions: element.getAttribute('max-options') || 100,
-                            plugins: {}
-                        };
-
-                        if (element.hasAttribute('remove-button-title')) {
-                            config.plugins.remove_button = {
-                                title: element.getAttribute('remove-button-title'),
-                                plugins: {
-                                    remove_button:{
-                                        title:'Remove this'
-                                    }
-                                },
-                            };
-                        }
-
-                        if (element.hasAttribute('clear-after-item-add')) {
-                            const clearAfterItemAddConfig = { 
-                                onItemAdd:function(){
-                                    this.setTextboxValue('');
-                                    this.refreshOptions();
-                                }
+                // Additional configuration based on attributes
+                if (s.hasAttribute('remove-button-title')) {
+                    config.plugins.remove_button = {
+                        title: s.getAttribute('remove-button-title'),
+                        plugins: {
+                            remove_button: {
+                                title: 'Remove this'
                             }
-                            config = {...config, ...clearAfterItemAddConfig};
                         }
-
-                        if (element.hasAttribute('raw-config')) {
-                            let rawConfig = JSON.parse(element.getAttribute('raw-config'));
-                            config = {...config, ...rawConfig};
-                        }
-                        console.info('htmx-tomselect - New TomSelect() config', config)
-                        new TomSelect(element, config)
-                    
-                        element.classList.add('tomselect-initialized');
-                    }
-                } catch (err) {
-                    console.error(`htmx-tomselect - Failed to load hx-tomsselect ${err}`)
+                    };
                 }
-            });
+
+                if (s.hasAttribute('clear-after-item-add')) {
+                    const clearAfterItemAddConfig = {
+                        onItemAdd: function() {
+                            this.setTextboxValue('');
+                            this.refreshOptions();
+                        }
+                    }
+                    config = {...config, ...clearAfterItemAddConfig};
+                }
+
+                if (s.hasAttribute('raw-config')) {
+                    let rawConfig = JSON.parse(element.getAttribute('raw-config'));
+                    config = {...config, ...rawConfig};
+                }
+
+                console.log('htmx-tomselect - v1 -  New TomSelect() config', config);
+
+                new TomSelect(s, config);
+                s.setAttribute('tom-select-success', "true");
+
+                })
+
+            } catch (err) {
+                s.setAttribute('tom-select-error', JSON.stringify(err));
+                console.error(`htmx-tomselect - Failed to load hx-tomsselect ${err}`);
+            }
         }
     }
 });
-console.info('htmx-tomselect - loaded!')
