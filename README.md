39b1eb9606c42fa12142978005f1a7cab2a7b788
KiwiKid <KiwiKid@users.noreply.github.com>
2024-04-19 08:54:35 +1200

    improvements

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Let me write the javascript so you dont have too
 [V0.1 - this is under active development, is provided as it, and future releases *may* introduce large breaking changes]
 [lmk if you see any bugs/feature oppotunitues]
 
-(should be loaded before any hx-ext tags)
 ```html
 <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet"/>
 <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
@@ -24,4 +23,31 @@ Example Usage:
 			</option>
 		}
 </select>
+
+
+hx-oob swaps works too!
+```html
+<div hx-swap-oob="true" id="player-select-potd" class="mt-4"> 
+	<label class="text-lg font-semibold">{ UsePlayerOfTheDayName(ctx) } ({ fmt.Sprintf("%d", len(players))  }):</label>
+		<select
+			hx-ext="tomselect"
+			max-options="20"
+			remove-button-title=""
+			name="playerOfTheDay"
+			class="mt-1 w-full border-gray-300  bg-white rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+		>
+			<option value="">N/A</option>
+			for _, p := range players {
+				<option
+					if p.ID == uint(playerId) {
+						selected
+					}
+					value={ S("%v", p.ID) }
+				>
+					{ S("%s", p.Name) }
+				</option>
+			}
+		</select>
+		}
+	</div>
 ```
--- hx-tom-select.js
+++ hx-tom-select.js
@@ -1,29 +1,103 @@
+const version = '02'
+
+/**
+ * @typedef {Object} ConfigChange
+ * Represents changes or settings that can be applied to TomSelect configuration.
+ * Each property name here should match a possible configuration key in TomSelect, and the type should match the expected configuration value type.
+ * @property {boolean} [persist] - Whether to keep selected items across page reloads.
+ * @property {boolean} [createOnBlur] - Whether to create a new item when input loses focus.
+ * @property {boolean} [create] - Whether to allow creation of new items not found in the options list.
+ * @property {boolean} [allowEmptyOption] - Whether to allow a select dropdown with no options.
+ * @property {SortField} [sortField] - Specifies how to sort items in the dropdown list.
+ */
+
+/**
+ * @typedef {Object} SortField
+ * Defines sorting options for TomSelect.
+ * @property {string} field - The field to sort by (e.g., 'text', 'value').
+ * @property {string} [direction] - The direction of the sort ('asc' or 'desc'). Omitting direction will sort in ascending order by default.
+ */
+
+/**
+ * @typedef {Object} SupportedAttribute
+ * Defines an attribute supported by a configuration modification system.
+ * @property {string} key - The key of the configuration attribute to modify.
+ * @property {ConfigChange} [configChange] - The modifications to apply to the TomSelect configuration.
+ */
+
+/**
+ * @type {SupportedAttribute[]}
+ */
+
+/**
+ * @typedef {'ts-max-options' | 'ts-create' | 'ts-sort' | 'ts-sort-direction' | 'ts-allow-empty-options', 'ts-clear-after-add', 'ts-raw-config'} TomSelectConfigKey
+ * Defines the valid keys for configuration options in TomSelect.
+ * Each key is a string literal corresponding to a specific property that can be configured in TomSelect.
+ */
+
+/**
+ * @type {Array<TomSelectConfigKey>}
+ */
+const attributes = ['ts-create', 'ts-max-options', 'ts-sort', 'ts-sort-direction', 'ts-allow-empty-options', 'ts-config', 'ts-clear-after-add', 'ts-raw-config']
+
 htmx.defineExtension('tomselect', {
     onEvent: function (name, evt) {
         if (name === "htmx:afterProcessNode") {
             try {
                 var elt = evt.detail.elt;
                 const selects = document.querySelectorAll('select[hx-ext="tomselect"]:not([tom-select-success]):not([tom-select-error])')
-                console.log('processing!'+selects.length)
                 selects.forEach((s) => {
+                    const allAttributes = s.attributes;
+                    const invalidAttrs = Array.from(allAttributes).filter(attr => !attributes.includes(attr.name) && attr.name.startsWith('ts-'));
+
+                    // Log a warning and mark element if invalid attributes are found
+                    invalidAttrs.forEach(attr => {
+                        console.warn(`Invalid config key found: ${attr.name}`);
+                        s.setAttribute('tom-select-warning', `Invalid config key found: ${attr.name}`);
+                    });
+                    /**
+                     * @type {ConfigChange}
+                     */
                     let config = {
-                        maxOptions: s.getAttribute('max-options') || 100,
+                        maxOptions: s.getAttribute('ts-max-options') || 100,
                         plugins: {}
                     };
 
-                // Additional configuration based on attributes
-                if (s.hasAttribute('remove-button-title')) {
-                    config.plugins.remove_button = {
-                        title: s.getAttribute('remove-button-title'),
-                        plugins: {
-                            remove_button: {
-                                title: 'Remove this'
-                            }
-                        }
-                    };
-                }
+                    if (s.hasAttribute('ts-create')) {
+                        config = {
+                            create: true,
+                            ...config
+                        };
+                    }
 
-                if (s.hasAttribute('clear-after-item-add')) {
+                    if(s.hasAttribute('ts-sort')){
+                        config = {
+                            sortField: {
+                                field: s.getAttribute('ts-sort'),
+                                direction: s.getAttribute('ts-sort-direction')
+                            },
+                            ...config
+                        };
+                    }
+
+                    if (s.hasAttribute('ts-allow-empty-options')) {
+                        config = {
+                            allowEmptyOption: true,
+                            ...config
+                        };
+                    }
+
+
+                    if (s.hasAttribute('ts-persist')) {
+                        config = {
+                            persist: true,
+                            ...config
+                        };
+                    }
+
+                   
+
+                if (s.hasAttribute('ts-clear-after-add')) {
                     const clearAfterItemAddConfig = {
                         onItemAdd: function() {
                             this.setTextboxValue('');
@@ -33,12 +107,25 @@ htmx.defineExtension('tomselect', {
                     config = {...config, ...clearAfterItemAddConfig};
                 }
 
-                if (s.hasAttribute('raw-config')) {
-                    let rawConfig = JSON.parse(element.getAttribute('raw-config'));
+                if (s.hasAttribute('ts-raw-config')) {
+                    let rawConfig = JSON.parse(s.getAttribute('ts-raw-config'));
                     config = {...config, ...rawConfig};
                 }
 
-                console.log('htmx-tomselect - v1 -  New TomSelect() config', config);
+
+
+                 // Additional configuration based on attributes
+                 if (s.hasAttribute('ts-remove-button-title')) {
+                    config.plugins.remove_button = {
+                        title: s.getAttribute('ts-remove-button-title'),
+                        plugins: {
+                            remove_button: {
+                                title: 'Remove this'
+                            }
+                        },
+                        ...config
+                    };
+                }
 
                 new TomSelect(s, config);
                 s.setAttribute('tom-select-success', "true");
